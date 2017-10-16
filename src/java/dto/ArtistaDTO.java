27c439a8c4748bd60004341c5e063a9dcc04e215
blamadrid <blamadrid@localhost>
2017-10-16 20:15:52 -0300

    Relleno de clases DTO

--- src/java/dto/ArtistaDTO.java
+++ src/java/dto/ArtistaDTO.java
@@ -16,10 +16,85 @@
  */
 package dto;
 
+import java.util.ArrayList;
+import java.util.List;
+import model.Album;
+
 /**
  *
  * @author benjamin
  */
 public class ArtistaDTO {
+    private List<Album> albumes;
+    private String nombre;
+    private int fechaNac;
+
+    /**
+     * Obtiene todos los álbumes del artista publicados.
+     * @return Un objeto ArrayList con dichos álbumes del artista.
+     */
+    public List<Album> getAlbumes() {
+        return albumes;
+    }
+
+    /**
+     * Ingresa una nueva entrada en la lista de álbumes del artista.
+     * @param album El objeto, de clase Album, a agregar a dicha lista.
+     */
+    public void addAlbum(Album album) {
+        this.albumes.add(album);
+    }
+
+    /**
+     * Elimina una entrada de la lista de álbumes del artista.
+     * @param index El número que identifica esta entrada.
+     */
+    public void removeAlbum(int index) {
+        this.albumes.remove(index);
+    }
+
+    /**
+     * Obtiene el nombre completo del artista.
+     * @return Un string con el nombre del artista.
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Establece el nombre del artista.
+     * @param nombre Un string conteniendo el nombre completo de dicho artista.
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Obtiene la fecha de nacimiento (o formación) del artista.
+     * @return Un string en formato 'DD/MM/AAAA'.
+     */
+    public int getFechaNac() {
+        return fechaNac;
+    }
+
+    /**
+     * Cambia la fecha de nacimiento establecida del artista.
+     * @param fecha Un String conteniendo una fecha en el formato 'DD/MM/AAAA'
+     */
+    public void setFechaNac(int fecha) {
+        this.fechaNac = fecha;
+    }
     
+    /**
+     * Crea un nuevo objeto Artista y le asigna una lista de álbumes ya creada.
+     * @param nombre Nombre completo del artista
+     * @param fecha Fecha, en formato 'DD/MM/AAAA'
+     * @param grupo true si es un grupo, false si es una persona
+     * @param albumes 
+     */
+    public ArtistaDTO(String nombre, int fecha, ArrayList<Album> albumes) {
+        this.albumes = albumes;
+        setNombre(nombre);
+        setFechaNac(fecha);
+    }
 }
